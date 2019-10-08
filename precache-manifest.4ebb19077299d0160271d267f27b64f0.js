1faedf59baa7f03753d418a08160b16db310dd6c
Chris Snyder <chris@chrissnyder.org>
2019-10-08 14:40:44 -0400

    Deploy

---
+++ precache-manifest.4ebb19077299d0160271d267f27b64f0.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e858a4c5d74f21422af",
+    "url": "bundle.css"
+  },
+  {
+    "revision": "55d04126817e358f1d8d6ea7cdf70b50",
+    "url": "index.html"
+  },
+  {
+    "revision": "d9f27af59f27a6924741",
+    "url": "js/1.d9f27af5.chunk.js"
+  },
+  {
+    "revision": "0e858a4c5d74f21422af",
+    "url": "js/app.0e858a4c.js"
+  },
+  {
+    "revision": "f492ab7ebdfb57ebffbd1a06f4600e79",
+    "url": "public/android-chrome-192x192.png"
+  },
+  {
+    "revision": "057e94f6ec397f66a96a4599d7a337a4",
+    "url": "public/android-chrome-512x512.png"
+  },
+  {
+    "revision": "9154811dd32d24e0d7cebc80ef17cdb6",
+    "url": "public/apple-touch-icon.png"
+  },
+  {
+    "revision": "9c65493050474b63eba50380529fc82d",
+    "url": "public/favicon-16x16.png"
+  },
+  {
+    "revision": "ba75a38587696280985e8e5c041ca7ea",
+    "url": "public/favicon-32x32.png"
+  },
+  {
+    "revision": "aaf76e50b940b29e95602017fe7b053c",
+    "url": "public/favicon.ico"
+  },
+  {
+    "revision": "62260c4c258febb095b621dc3b2fba9f",
+    "url": "public/images/purple_blob.svg"
+  },
+  {
+    "revision": "dedb66f7c62c1a52604b6bbde91c3f27",
+    "url": "public/manifest.json"
+  }
+]);
